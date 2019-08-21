849651483d02d385d18b5b13d76b6f3a23772700
Gurdain <47345168+Gurdain@users.noreply.github.com>
2019-08-21 11:55:45 +0530

    Add files via upload

---
+++ TimeSheetReport/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TimeSheetReport
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Timers.Timer time = new System.Timers.Timer();
+            time.Start();
+            time.Interval = 60000; // 1 min //3.6e+6; // 1 hour
+            time.Elapsed += time_elapsed;
+        }
+
+        public void time_elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            @TimeSheetReport.Controllers.HomeController.CheckTask();
+            @TimeSheetReport.Controllers.HomeController.SendEmailFromQueue();
+        }
+    }
+}
